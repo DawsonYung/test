e11212f92b9d22d83b45bcb0774520fa00b712e3
yangdesheng <18335101167@163.com>
2018-07-26 13:11:04 +0800

    【REQMGM-004】集成thymeleaf 测试，暂时未成功，contoller return不到templates下

--- src/main/java/com/yangdsh/SpringApplicationStart.java
+++ src/main/java/com/yangdsh/SpringApplicationStart.java
@@ -4,12 +4,14 @@ package com.yangdsh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Springboot 启动入口
  */
 @SpringBootApplication
 @MapperScan("com.yangdsh.mapper")
+@ComponentScan("com.yangdsh")
 public class SpringApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationStart.class, args);
